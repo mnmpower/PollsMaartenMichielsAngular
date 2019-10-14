5681d5d0f789690b2524c7f2b0b5b1c746049de7
mnmpower <mnmmaarten@hotmail.com>
2019-10-14 22:25:39 +0200

    added login page withoutveryfication on submit

--- src/app/Authentication/login/login.component.ts
+++ src/app/Authentication/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+// import { AuthenticateService } from 'src/app/Services/authenticate.service';
+
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  submitted: boolean = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router) {
+  }
+
+  loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(2)]]
+  });
+
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.submitted);
+    // console.log(this.loginForm.value);
+    // this._authenticateService.authenticate(this.loginForm.value).subscribe(result => {
+    //   localStorage.setItem("token", result.token);
+    //   this._authenticateService.isLoggedin.next(result.token ? true : false);
+    //
+    //   this.router.navigate(['../home'], { replaceUrl: true });
+    // });
+  }
 }
